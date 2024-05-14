1ce931a84be8c8c4998f816197fb376db19754e1
dayounglee <verdantjuly@kakao.com>
2024-05-14 20:10:57 +0900

    [Refactor] ssl 사용 설정 해제

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Errortoday
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.secret_key_base = '<%= ENV["SECRET_KEY_BASE"] %>'
   end
 end
